2de639d478d19c17b9dbb6b0b28b7dcfa31f0990
Siebe Hoogenboom <shoogenb@student.codam.nl>
2024-06-04 15:38:48 +0200

    cleanupp

--- src/cpu/PixelProcessingUnit.cpp
+++ src/cpu/PixelProcessingUnit.cpp
@@ -137,12 +137,10 @@ void PixelProcessingUnit::handle_interrupt(bool val, MemoryMap &mmap) {
 }
 
 uint8_t PixelProcessingUnit::get_pixel(uint8_t tile_index, uint8_t x, uint8_t y, MemoryMap &mmap) {
-    uint8_t vbank = mmap.vram_bank_select() ? 1 : 0; //TODO if this is correct
+    uint8_t vbank = mmap.vram_bank_select() ? 1 : 0;
     uint8_t *t = mmap.get_tile_data(vbank, tile_index);
     uint8_t bottom = t[(y * 2 + 1)];
     uint8_t top = t[(y * 2)];
-    // std::cout << "bottom: " << (uint16_t)bottom << ", top: " << (uint16_t)top << std::endl;
-    // std::cout << "tile index: " << (uint16_t)tile_index << std::endl;
     uint8_t pixel = ((top & (0x80 >> x)) == 0) ? 0x00 : 0b10;
     pixel |= ((bottom & (0x80 >> x)) == 0) ? 0x00 : 0b01;
     return pixel;
@@ -151,35 +149,17 @@ uint8_t PixelProcessingUnit::get_pixel(uint8_t tile_index, uint8_t x, uint8_t y,
 void PixelProcessingUnit::fill_pixels(std::deque<std::array<uint8_t, 2>> &pixels, uint16_t x, uint8_t n, uint8_t y, MemoryMap &mmap) {
     uint8_t vbank = mmap.vram_bank_select() ? 1 : 0; //TODO if this is correct
     uint16_t tile_map = mmap.get_background_tilemap() ? 0x1c00 : 0x1800;
-    uint32_t line_offset = mmap.get_lcd_scrolling_x() >> 3;
-    uint16_t map_number;
-    if (mmap.get_bg_window_tiles()) {
-            map_number = (64 - y / 8);
-        } else {
-            map_number = (y / 8);
-        }
     // std::cout << "map number: " << map_number << std::endl;
     // tile_map += (((mmap.get_lcd_line_y() + mmap.get_lcd_scrolling_y()) & 255) >> 3) << 5;
     for (uint16_t p_x = x; p_x < (x + (uint16_t)n); p_x++) {
         if (mmap.get_window_enable() && mmap.get_lcd_window_x() == 0 + 7) {
             tile_map = (mmap.get_window_tilemap() ? 0x1C00 : 0x1800) + ((mmap.get_lcd_window_y() >> 3) << 5);
-            // line_offset = 0;
         }
         uint8_t tile_index = mmap.get_tile_index(vbank, (p_x % 255) / 8, y / 8, tile_map);
-        // std::cout << "map_offset: " << tile_map << " line_offset: " << line_offset << " tile_index: " << (uint16_t)mmap.vram[0][tile_map + line_offset] << std::endl;
         // if (!mmap.get_bg_window_tiles()) {
             //TODO check if this is needed?
             // tile_map = (384 - tile_map as u16) as u8;
         // }
-        // if (tile_index)
-        //     std::cout << "tile index: " << (uint16_t)tile_index << std::endl;
-        // std::cout << "px: " << (uint16_t)p_x << ", y: " << (uint16_t)y << std::endl;
-        // std::cout << "tile_map: " << (uint16_t)tile_map << std::endl;
-        // std::cout << "xy : " << (uint16_t)((p_x % 255) / 8) << ", " << (uint16_t)(y / 8) << std::endl;
-        // uint16_t x_pos = (uint16_t)((p_x % 255) / 8);
-        // uint16_t y_pos = (uint16_t)(y / 8);
-        // std::cout << "vram index: " << std::hex << 0x8000 + 0x1800 + (x_pos + y_pos * 32 + 32 * (uint16_t)tile_map * 32) << std::dec << std::endl;
-        // std::cout << "vram size: " << mmap.vram[0].size() << std::endl;
         std::array<uint8_t, 2> pixel; 
         pixel[0] = get_pixel(tile_index, (p_x % 255) % 8, y % 8, mmap);
         pixel[1] = 0;
@@ -188,65 +168,25 @@ void PixelProcessingUnit::fill_pixels(std::deque<std::array<uint8_t, 2>> &pixels
 }
 
 void PixelProcessingUnit::set_pixel(uint32_t x, uint32_t y, uint32_t pixel, MemoryMap &mmap) {
-    // std::cout << "set trying to set pixel: " << x <<", " << y<< std::endl;
-    // std::cout << "pixels: " << data.screen->w << ", " << data.screen->h << ", pitch: " << data.screen->pitch << std::endl;
-    // std::cout << "pixel offset: " << y * data.screen->pitch
-    //                                          + x * data.screen->format->BytesPerPixel << std::endl;
-    // std::cout << "x: " << x << ", y: " << y << std::endl;
-    // uint32_t * const target_pixel = (uint32_t *) ((uint8_t *) data.screen->pixels
-    //                                          + y * data.screen->pitch
-    //                                          + x * data.screen->format->BytesPerPixel);
-    // uint32_t * const target_pixel = (uint32_t *) ((uint8_t *) data.surface->pixels
-    //                                          + y * data.surface->pitch
-    //                                          + x * data.surface->format->BytesPerPixel);
     uint32_t color = 0x000000;
-    uint32_t val = 0;
     switch (pixel) {
         case 0b00:
-            val = mmap.get_background_palette_data() & 0b11;
-            break;
-        case 0b01:
-            val = (mmap.get_background_palette_data() & 0b1100) >> 2;
-            break;
-        case 0b10:
-            val = (mmap.get_background_palette_data() & 0b110000) >> 4;
-            break;
-        case 0b11:
-            val = (mmap.get_background_palette_data() & 0b11000000) >> 6;
-            break;
-        default:
-            break;
-    }
-    // if (val)
-    // std::cout << "val: " << val << std::endl;
-    switch (pixel) {
-        case 0b00:
-            color = 0xffe7cdff;
+            color = 0xff0000ff;
         break;
         case 0b01:
-        color = 0xe4a39fff;
+        color = 0x00FF00ff;
         break;
         case 0b10:
-        color = 0x629098ff;
+        color = 0x0000FFff;
         break;
         case 0b11:
-        color = 0x4c3457ff;
+        color = 0xFFFFFFff;
         break;
         default:
         color = 0x000000;
         break;
     }
     data.framebuffer[x + y * 160] = color;
-    // SDL_SetRenderDrawColor(data.renderer, (uint8_t)(color << 24), (uint8_t)(color << 16), (uint8_t)(color << 8), 255);
-    // SDL_RenderDrawPoint(data.renderer, x, y);
-    // if (color != 0xFFFFFF)
-    //     std::cout << "color: " << std::hex << color << std::dec << std::endl;
-    // *target_pixel = color;
-    // uint8_t* pixels = (uint8_t*)data.screen->pixels;
-    // pixels[4 * (y * data.screen->pitch + x) + 0] = pixel;
-    // pixels[4 * (y * data.screen->pitch + x) + 1] = pixel << 8;
-    // pixels[4 * (y * data.screen->pitch + x) + 2] = pixel << 16;
-    // pixels[4 * (y * data.screen->pitch + x) + 3] = pixel << 24;
 }
 
 void PixelProcessingUnit::render_scanline(MemoryMap &mmap) {
@@ -260,7 +200,6 @@ void PixelProcessingUnit::render_scanline(MemoryMap &mmap) {
     // std::cout << "0x1c00 - 0x1800: " << 0x1c00 - 0x1800 << std::endl;
     uint32_t start_x = mmap.get_lcd_scrolling_x();
     fill_pixels(pixels, start_x, 16, start_y, mmap);
-    // std::cout << "pixels filled: " << pixels.size() << std::endl;
     if (mmap.get_obj_enable()) {
         uint8_t ly = mmap.get_lcd_line_y();
         for (int i = 0; i < 40; i++) {
@@ -274,14 +213,10 @@ void PixelProcessingUnit::render_scanline(MemoryMap &mmap) {
             }
         }
     }
-    if (sprites.size())
-    std::cout << "size: " << sprites.size() << std::endl;
-    // std::cout << "pixels filled: " << pixels.size() << std::endl;
     for (uint32_t x = 0; x < 160; x++) {
         while (pixels.size() < 8) {
             start_x = (x + mmap.get_lcd_scrolling_x() + 8);
             fill_pixels(pixels, start_x, 8, start_y, mmap);
-            // std::cout << "pixels filled: " << pixels.size() << std::endl;
         }
         uint8_t spr_num = 0;
 
@@ -323,20 +258,11 @@ void PixelProcessingUnit::render_scanline(MemoryMap &mmap) {
         set_pixel(x, static_cast<uint32_t>(mmap.get_lcd_line_y()), pixels[0][0], mmap);
         pixels.pop_front();
     }
-    // std::cout << "pixels filled: " << pixels.size() << std::endl;
-    // std::cout << "ly: " << (uint16_t)mmap.get_lcd_line_y() << std::endl;
-    // std::cout << "trying to set pixel: " << 0 <<", " << static_cast<uint32_t>(mmap.get_lcd_line_y()) * 160<< std::endl;
-    // data.screen->pixels[y * 160] = pixels[0][0];
     pixels.clear();
     sprites.clear();
 }
 
 void PixelProcessingUnit::render_screen() {
-    // SDL_BlitSurface(data.screen, NULL, data.surface, NULL);
-    // SDL_UpdateWindowSurface(data.window);
-    //TODO check what are best functions to do this stuff?
-    // SDL_SetRenderDrawColor(data.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    // SDL_RenderClear(data.renderer);
     SDL_RenderPresent(data.renderer);
     SDL_UpdateTexture(data.texture, NULL, data.framebuffer, 160 * sizeof(uint32_t));
     SDL_RenderClear(data.renderer);
@@ -375,13 +301,6 @@ bool PixelProcessingUnit::init_window()
                 printf("texture could not be created! SDL_Error: %s\n", SDL_GetError());
                 return false;
             }
-            // Get window surface
-            // data.surface = SDL_GetWindowSurface(data.window);
-            // data.screen = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
-            // if (data.screen == NULL || data.surface == NULL) {
-            //     printf("Screen surface could not be created! SDL_Error: %s\n", SDL_GetError());
-            //     success = false;
-            // }
         }
     }
     for (int i = 0; i < 144*160; i++) {
@@ -392,10 +311,6 @@ bool PixelProcessingUnit::init_window()
 }
 
 void PixelProcessingUnit::close() {
-    // SDL_FreeSurface(data.screen);
-    // data.screen = NULL;
-    // SDL_FreeSurface(data.surface);
-    // data.surface = NULL;
     SDL_DestroyTexture(data.texture);
 
     SDL_DestroyRenderer(data.renderer);
