b89a1758025af6c5759e9bb72f74b35affff9256
Siebe Hoogenboom <shoogenb@student.codam.nl>
2024-07-04 16:23:35 +0200

    some cleanup and todo's

--- src/cpu/PixelProcessingUnit.cpp
+++ src/cpu/PixelProcessingUnit.cpp
@@ -14,25 +14,20 @@ PixelProcessingUnit::PixelProcessingUnit(Cpu *cpu) : cpu(cpu) {
 	ctrl = {0};
 	l_status = {0};
 	l_status.mode = PPU_Modes::Pixel_Drawing;
-	// sprites = {0};
 	std::memset(mono_framebuffer, 0, sizeof(mono_framebuffer));
 	std::memset(rgb_framebuffer, 0, sizeof(rgb_framebuffer));
 	std::memset(r5g6b6_framebuffer, 0, sizeof(r5g6b6_framebuffer));
 	std::memset(vram, 0, sizeof(vram));
 	std::memset(oam, 0, sizeof(oam));
-	// std::memset(obj_0_colors, 0, sizeof(obj_0_colors));
-	// std::memset(obj_1_colors, 0, sizeof(obj_1_colors));
 	std::memset(tile_data, 0, sizeof(tile_data));
 	dma = {0};
 	is_cgb = cpu->get_mmap().is_cgb_rom();
 	printf("is cgb_rom: %u\n", is_cgb);
-	// std::memset(cgb_bg_colors, 0, sizeof(cgb_bg_colors));
 }
 
 PixelProcessingUnit::~PixelProcessingUnit() {}
 
 void PixelProcessingUnit::tick(uint16_t &cycle) {
-	// printf("mode: %u clock: %u cycle: %u\n", l_status.mode, lcd_clock, cycle);
 	lcd_clock += cycle;
 	if (lcd_enabled) {
 
@@ -174,6 +169,7 @@ void PixelProcessingUnit::handle_pixel_drawing(uint16_t &cycle) {
 		tile_drawn += cycle;
 		if (lcd_enabled && ctrl.lcd_enable) {
 			while (tile_drawn >= 3) {
+				//TODO check if this is needed
 				render_background(ly, pixels_drawn);
 				pixels_drawn += 4;
 				tile_drawn -= 3;
@@ -245,6 +241,7 @@ void PixelProcessingUnit::dma_transfer(uint8_t cycle) {
 	}
 }
 
+//TODO clean this up and optimize it more
 void PixelProcessingUnit::render_background(uint8_t line, uint8_t pixel) {
 	int line_width = line * 160;
 	uint8_t y = 0;
@@ -326,6 +323,7 @@ void PixelProcessingUnit::render_background(uint8_t line, uint8_t pixel) {
 	}
 }
 
+//TODO clean this up and optimize it more
 void PixelProcessingUnit::render_window(uint8_t line) {
 	if (window_line_active > 143) {
 		return;
@@ -415,6 +413,8 @@ void PixelProcessingUnit::render_window(uint8_t line) {
 	window_line_active++;
 }
 
+//TODO clean this up and optimize it more
+//TODO maybe bring back the sprites instead of the oam stuff?
 void PixelProcessingUnit::render_sprites(uint8_t line) {
 	if (!ctrl.obj_enable)
 		return;
@@ -424,6 +424,7 @@ void PixelProcessingUnit::render_sprites(uint8_t line) {
 
 	bool visible_sprites[40];
 	int sprite_limit = 0;
+	int y = 0;
 
 	for (int sprite = 0; sprite < 40; sprite++) {
 		int sprite_4 = sprite << 2;
@@ -482,11 +483,21 @@ void PixelProcessingUnit::render_sprites(uint8_t line) {
 			byte2 = read_u8_ppu(tile_address + 1);
 		}
 
+		uint16_t addr = tile_address;
+		addr &= 0x1FFE;
+		y = (addr >> 1) & 7;
+		if (yflip) {
+			y = 7 - y;
+		}
+		uint8_t tile_dat;
+		int tile_index = (addr >> 4) & 0x1FF;
 		for (int pixelx = 0; pixelx < 8; pixelx++) {
-			int pixel = (byte1 & (0x01 << (xflip ? pixelx : 7 - pixelx))) ? 1 : 0;
-			pixel |= (byte2 & (0x01 << (xflip ? pixelx : 7 - pixelx))) ? 2 : 0;
-
-			if (pixel == 0)
+			if (cgb_tile_bank) {
+				tile_dat = tile_data[1][tile_index][y * 8 + (!xflip ? pixelx : 7 - pixelx)];
+			} else {
+				tile_dat = tile_data[vbank_select][tile_index][y * 8 + (!xflip ? pixelx : 7 - pixelx)];
+			}
+			if (tile_dat == 0)
 				continue;
 
 			int bufferX = (sprite_x + pixelx);
@@ -512,9 +523,9 @@ void PixelProcessingUnit::render_sprites(uint8_t line) {
 			color_cache_buffer[position] = (color_cache & mask3);
 			sprite_cache_buffer[position] = sprite_x;
 			if (is_cgb) {
-				r5g6b6_framebuffer[position] = cgb_obj_colors[cgb_tile_pal][pixel][1];
+				r5g6b6_framebuffer[position] = cgb_obj_colors[cgb_tile_pal][tile_dat][1];
 			} else {
-				uint8_t color = (palette >> (pixel << 1)) & 0x03;
+				uint8_t color = (palette >> (tile_dat << 1)) & 0x03;
 				r5g6b6_framebuffer[position] = mono_framebuffer[position] = color;
 			}
 		}
@@ -522,89 +533,6 @@ void PixelProcessingUnit::render_sprites(uint8_t line) {
 }
 
 void PixelProcessingUnit::render_scanline(uint8_t line) {
-	// std::vector<std::reference_wrapper<Sprite>> sprites_sorted;
-
-	// if (ctrl.obj_enable) {
-	// 	sprites_sorted.reserve(10);
-	// 	for (Sprite &spr : sprites) {
-	// 		if (ly < spr.y_pos &&
-	// 		    ly >= spr.y_pos - 16) { // obj can be 8*8 or 8*16 //TODO maybe do a obj_size check that returns 8 or 16
-	// 			if (ctrl.obj_size || ly < spr.y_pos - 8) {
-	// 				sprites_sorted.push_back(spr);
-	// 				if (sprites_sorted.size() == 10)
-	// 					break;
-	// 			}
-	// 		}
-	// 	}
-
-	// 	// TODO do this in a differnt way
-	// 	std::stable_sort(sprites_sorted.begin(), sprites_sorted.end(),
-	// 	                 [](const std::reference_wrapper<Sprite> &a, const std::reference_wrapper<Sprite> &b) {
-	// 		                 return (a.get().x_pos < b.get().x_pos);
-	// 	                 });
-	// 	std::erase_if(sprites_sorted, [](const auto &a) { return (a.get().x_pos <= 0 || a.get().x_pos >= 168); });
-	// }
-
-	// int tile_map_offset = ctrl.bg_tile_map_address ? 0x1c00 : 0x1800;
-	// int line_offset = scx >> 3;
-	// uint8_t y = (ly + scy) & 7;
-	// uint8_t x = scx & 7;
-
-	// tile_map_offset += (((ly + scy) & 255) >> 3) << 5;
-	// if (window_active && window_x == 0 + 7) {
-	// 	tile_map_offset = (ctrl.window_tile_map_address ? 0x1c00 : 0x1800) + ((window_line_active >> 3) << 5);
-	// 	line_offset = 0;
-	// 	y = window_line_active & 7;
-	// 	x = 0;
-	// }
-
-	// uint32_t *framebuffer_ptr = framebuffer + ly * 160;
-	// uint16_t tile_index = vram[vbank_select][tile_map_offset + line_offset];
-	// if (!ctrl.bg_window_tile_data) {
-	// 	tile_index = static_cast<uint16_t>(256 + static_cast<int8_t>(tile_index & 0xFF));
-	// }
-
-	// size_t spr_index = 0;
-	// for (uint32_t i = 0; i < 160; i++) {
-	// 	uint8_t tile_dat = tile_data[vbank_select][tile_index][(uint16_t)(y) * 8 + x];
-	// 	if (is_cgb) {
-	// 		uint16_t tile_attr_index = vram[1][tile_map_offset + line_offset];
-	// 		uint8_t tile_attr = tile_data[1][tile_attr_index][(uint16_t)(y) * 8 + x];
-	// 		uint8_t tile_pal = tile_attr & 0b111;
-	// 		// if (tile_dat) {
-	// 		// 	printf("tile_dat: %u tile_attr: %u tile_pal: %u\n", tile_dat, tile_attr, tile_pal);
-	// 		// }
-	// 		*framebuffer_ptr = cgb_bg_colors_other_32[tile_pal][tile_dat];
-	// 	} else {
-	// 		*framebuffer_ptr = bg_colors[tile_dat];
-	// 	}
-
-	// 	handle_sprites(sprites_sorted, i, tile_dat, framebuffer_ptr, &spr_index);
-	// 	framebuffer_ptr++;
-	// 	x++;
-	// 	if (x == 8) {
-	// 		x = 0;
-	// 		line_offset = (line_offset + 1) & 31;
-	// 		tile_index = vram[vbank_select][tile_map_offset + line_offset];
-	// 		if (!ctrl.bg_window_tile_data) {
-	// 			tile_index = static_cast<uint16_t>(256 + static_cast<int8_t>(tile_index & 0xFF));
-	// 		}
-	// 	}
-	// }
-	// if (window_active) {
-	// 	window_line_active++;
-	// }
-
-	/* TODO CGB COLOR STUFF
-	    needs:
-	        - color_index (tile_dat?)
-	        - palette_nb (Option if None -> Default color) otherwise (bits & 0b111) where bits is from tile_index from
-	   bank1?
-	        - is_background (if sprite this is false)
-
-	    //Also need to handle mixing of colors (only for background)
-	*/
-
 	if (lcd_enabled && ctrl.lcd_enable) {
 		render_background(line, 0);
 		render_window(line);
